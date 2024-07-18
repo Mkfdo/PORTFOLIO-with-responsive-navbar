9fa109e76467a4cdc5bac727a91a50d228d3de55
Mkfdo <mithila201213@gmail.com>
2024-07-18 15:38:50 +0530

    Navbar linking

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import logo from '../components/pages/assets/logo.png'
+import './Navbar.css';
+
 
 export const Navbar = () => {
   return (
-    <nav>
+    <nav className='navbar'>
+        <img src={logo} alt='logo' className='logo'/>
+        <div className='desktopMenu'>
+            <Link to='/intro' className='desktopMenuListItem'>Home Page</Link>
+            <Link to='/about' className='desktopMenuListItem'>About Me</Link>
+            <Link to='/project' className='desktopMenuListItem'>Projects</Link> 
+            <Link to='/contact' className='desktopMenuListItem'>Contact Me</Link>
+        </div>
         
-        <ul>
-            <li>
-                <Link to='/intro'>Home Page</Link>
-            </li>
-            <li>
-                <Link to='/about'>About Me</Link>
-            </li>
-            <li>
-                <Link to='/project'>Projects</Link>
-            </li>
-            <li>
-                <Link to='/contact'>Contact Me</Link>
-            </li>
-        </ul>
+            
+                    
+                
+        
     </nav>
   );
 };
