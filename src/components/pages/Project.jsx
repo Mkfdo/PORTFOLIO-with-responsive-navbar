464e33fe7679c0287329c8ef1ce786574202a9fe
Mkfdo <mithila201213@gmail.com>
2024-07-18 15:41:02 +0530

    Updates the projects I have already done

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './Project.css';
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <section id='project'>
+        <h1 className='projectTitle'><b><i>My Projects</i></b></h1>
+        <span className='projectDesc'>This is the Project I have done.</span>
+        <div className='projectBars'>
+            <div className='projectBar'>
+                <div className='projectBarText'>
+                    <h3>01. Login & Signup Form using React</h3><br/>
+                    <Link rel='githublink' href="https://github.com/Mkfdo/sign-in-attempt-3">Github Login form</Link>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
