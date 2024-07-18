e72fd9771e005a85f2500a9c3699fd534b3d52fc
Mkfdo <mithila201213@gmail.com>
2024-07-18 15:39:48 +0530

    WIP contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Contact.css';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id='contactPage'>
+        <div className='pageTitle'><h1>Contact Me</h1>
+            <span className='pageDesc'>Please fill out the form below to discuss any work opportunities.</span>
+            <form className='contactForm'>
+              <input type='text' className='name' placeholder='Your Name' />
+              <input type='email' className='email' placeholder='Your Email' />
+              <textarea className='msg' name='message' rows="5" placeholder='Your Message'></textarea>
+              <button type='submit' value='send' className='submitBtn'>Submit</button>
+            </form>
+        </div>
+    </section>
   )
 }
 
