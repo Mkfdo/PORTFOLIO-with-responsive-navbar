019cd1674e28f28e450fc1044210a6b89ef4bb62
Mkfdo <mithila201213@gmail.com>
2024-07-19 11:32:13 +0530

    Update the changes of 404 Error page and default first page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Intro from './components/pages/Intro';
 import About from './components/pages/About';
 import Project from './components/pages/Project';
 import Contact from './components/pages/Contact';
+import Nopage from './components/pages/Nopage';
 
 
 
@@ -13,10 +14,12 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
+        <Route index element={<Intro />} />
         <Route path='/intro' element={<Intro />} />
         <Route path='/about' element={<About/>} />
         <Route path='/project' element={<Project />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<Nopage />} />
 
       </Routes>
     </div>
